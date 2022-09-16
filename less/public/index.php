f17355aec2f8ab5a5fbbda00e3c3ddc9e2c11fba
Kevin Lizarraga <64446733+Quebny@users.noreply.github.com>
2022-09-15 18:48:40 -0600

    4.4 (Login con less)

---
+++ less/public/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel</title>
+    <link rel="stylesheet" type="text/css" href="css/main.less">
+</head>
+
+<body>
+    <div class="container">
+        <fieldset>
+            <form>
+                <legend>
+                    Acceso al panel
+                </legend>
+
+                <label>
+                    Email
+                </label>
+
+                <input type="email" name="" required>
+
+                </legend>
+                <label>
+                    Contraseña
+                </label>
+
+                <input type="password" name="" required>
+
+                <button type="submit">
+                    Login
+                </button>
+            </form>
+        </fieldset>
+    </div>
+</body>
+
+</html>
