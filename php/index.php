44080516df7fb0f18f3e57f61e57ee7b3046db78
Quebny <quebynliga@gmail.com>
2022-09-29 13:14:56 -0500

    Ejercicio 4.14 PHP Layouts

---
+++ php/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" type="text/css" href="css/main.less"> -->
+    <style type="text/css">
+        .recolor {
+            background-color:steelblue
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container recolor mt-10">
+        <section>
+            <div class="row align-self-center justify-content-md-center">
+                <div class="col-md-4 col-sm-12">
+                    <fieldset>
+                        <form>
+                            <h1 class="text-center">
+                                Login
+                            </h1>
+
+                            <label>
+                                Email
+                            </label>
+
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">@</span>
+                                <input type="text" class="form-control" placeholder="example@gmail.com" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+
+
+                            <label>
+                                Contraseña
+                            </label>
+
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon2">#</span>
+                                <input type="text" class="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon2">
+                            </div>
+
+                            <div class="d-grid gap-2 col-4   mx-auto">
+                                <a class="btn btn-primary" href="products/index.php" type="button">Login</a>
+                            </div>
+                        </form>
+                    </fieldset>
+                </div>
+            </div>
+        </section>
+
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js">
+
+    </script>
+</body>
+
+</html>
---
+++ php/products/index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../layout/head.template.php'; ?>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <?php include '../layout/nav.template.php'; ?>
+
+    <!-- Sidebar -->
+    <div class="container-fluid">
+        <div class="row">
+
+            <?php include '../layout/sidebar.template.php'; ?>
+
+            <!-- Content -->
+
+            <div class="col-md-10 col-lg-10 col-sm-12">
+
+                <section>
+                    <div class="row bg-light m-2">
+                        <div class="col">
+                            <label>
+                                Productos
+                            </label>
+                        </div>
+                        <div class="col">
+                            <button data-bs-toggle="modal" data-bs-target="#addProductModal" class=" float-end btn btn-primary">
+                                Añadir producto
+                            </button>
+                        </div>
+                    </div>
+                </section>
+
+                <section>
+
+                    <div class="row">
+
+                        <?php for ($i = 0; $i < 9; $i++) : ?>
+
+                            <div class="col-md-4 col-sm-12">
+
+                                <div class="card mb-2">
+                                    <img src="../public/img/logo.png" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Producto</h5>
+                                        <h6 class="card-subtitle mb-2 text-muted">Precio (?)</h6>
+                                        <p class="card-text">Descripción / Detalles</p>
+
+                                        <div class="row">
+                                            <a data-bs-toggle="modal" data-bs-target="#addProductModal" href="#" class="btn btn-warning mb-1 col-6">
+                                                Editar
+                                            </a>
+                                            <a onclick="eliminar(this)" href="#" class="btn btn-danger mb-1 col-6">
+                                                Eliminar
+                                            </a>
+                                            <a href="detalles.php" class="btn btn-info col-12">
+                                                Detalles
+                                            </a>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        <?php endfor; ?>
+
+                    </div>
+
+                </section>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="addProductModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Acción</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <form>
+
+                    <div class="modal-body">
+
+                        <?php for ($i = 0; $i < 4; $i++) : ?>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">!</span>
+                                <input required type="text" class="form-control" placeholder="Input" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+                        <?php endfor; ?>
+
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                            Cerrar
+                        </button>
+                        <button type="submit" class="btn btn-primary">
+                            Guardar cambios
+                        </button>
+                    </div>
+
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- Scripts -->
+    <?php include '../layout/scripts.template.php'; ?>
+</body>
+
+</html>
