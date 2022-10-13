432c7230d5d767e8aadb7d301a3130d89ad4300f
Quebny <quebynliga@gmail.com>
2022-10-13 18:57:31 -0500

    5.3 Rutas Laravel

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
+use Illuminate\Routing\Route;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/saludo', function(){
+    return view('Hola');
+});
+
+Route::get('/saludo/{name}', function($name){
+    return 'Hola '.$name;
+});
+
+Route::get('/suma/{num1}/{num2}', function($num1, $num2){
+    return $num1 + $num2;
+}) -> where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
+
+Routte:get('/mult/{num1}/{num2}/{num3?}', function($num1, $num2, $num3 = 1){
+    return $num1 * $num2 * $num3;
 });
